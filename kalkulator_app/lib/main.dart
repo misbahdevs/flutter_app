0d80996e86f9b7d97ebe411ac4db3fb0001859f0
misbahdev <misbahx.id@gmail.com>
2023-11-05 10:38:27 +0700

    add: flutter kalkulator app

---
+++ kalkulator_app/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:kalkulator_app/calculator_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Calculator(),
+    );
+  }
+}
